c06524f186c69388498e0b9ae33c0d7d779ef679
Taylor <tayloryoung@mvmediasales.com>
2019-12-11 14:50:19 -0500

    edit app.js

--- src/App.js
+++ src/App.js
@@ -9,31 +9,11 @@ class App extends Component {
   constructor(){
     super()
     this.state = {
-      instructions: 'Choose From Dropdown',
-      selection: 1,
       tableDisplayed: null
     }
   }
   
-  setFunction = (e, data) => {
-    e.preventDefault()
-    const selection = data.options[data.value - 1]
-    this.setState({
-      selection: selection.value,
-      instructions: ''
-    })
-  }
-
-  renderTableStyle = () => {
-    this.setState({tableDisplayed: true})
-  }
-
-
   render(){
-
-    const instructionsStyle = this.state.instructions.length > 0 ? instPresent : null
-    const appStyle = this.state.tableDisplayed ? tableAppStyle : defaultAppStyle
-
     return (
       <div className="App" style={appStyle}>
           <div className="text-box">
@@ -42,8 +22,8 @@ class App extends Component {
               <span class="heading-primary-sub">CSV Parser</span>
             </h1>
           </div>
-          <div className="body-wrapper">
-            {this.state.selection === 1 ? <CSVParser tableFx={this.renderTableStyle}/>  : null}
+          <div className="main-wrapper">
+            <CSVParser />
           </div>
         </div>
     );
@@ -52,30 +32,7 @@ class App extends Component {
 
 export default App
 
-
-
-
-
-
-
-const instPresent = {
-  width: '200px'
-}
-
-const tableAppStyle = {
-  height: '50%',
-  width: '100%',
-  margin: '0 auto',
-  padding: '20px',
-  position: 'absolute',
-  top: '0',
-  alignItems: 'center',
-  resize: 'both',
-  overflow: 'none',
-  borderRadius: '5px'
-}
-
-const defaultAppStyle = {
+const appStyle = {
   height: '50%',
   width: '100%',
   margin: '0 auto',
